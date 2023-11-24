50bd3e2636c75d182d4bc004010c79cd16be0627
及川浩太朗 <oikawakoutarou@oikawakoutarous-MacBook-Pro.local>
2023-11-24 15:24:36 +0900

    ユニーク更新

--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,8 @@
 from app import db
 
 class Timetable(db.Model):
-    date = db.Column(db.String(10), primary_key=True)
-    period = db.Column(db.Integer, primary_key=True)
-    classname = db.Column(db.String(100))
-    professor = db.Column(db.String(15))
-    room = db.Column(db.String(15))
+    date = db.Column(db.String(10), primary_key=True, unique=False)
+    period = db.Column(db.Integer, primary_key=True, unique=False)
+    classname = db.Column(db.String(100), unique=False)
+    professor = db.Column(db.String(15), unique=False)
+    room = db.Column(db.String(15), unique=False)
